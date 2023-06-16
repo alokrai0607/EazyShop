79d796c3713b3663db1078ac3aff12545d8630c2
alokrai0607 <saelalok001@gmail.com>
2023-06-16 22:21:06 +0530

    Day-3

---
+++ EazyBuy/src/main/java/com/EazyBuy/repository/AdminRepository.java
@@ -0,0 +1,13 @@
+package com.EazyBuy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.EazyBuy.model.Admin;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Integer>{
+
+	public Admin findByEmail(String email);
+	
+}
